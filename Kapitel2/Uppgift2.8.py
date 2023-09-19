266fc80111b31f32e58ffa2adcb6823e2001a177
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:20:08 +0200

    Delete Kapitel2/Uppgift2.8.py

--- Kapitel2/Uppgift2.8.py
+++
@@ -1,8 +0,0 @@
-import random
-# ett program som kastar tärningar, 2 i de här fallet
-print("Roll the dice!")
-n = random.randint (1,6)
-b = random.randint (1,6)
-all = n + b
-print(f"You rolled {n}, {b}")
-print(f"You rolled total of: ", all)
