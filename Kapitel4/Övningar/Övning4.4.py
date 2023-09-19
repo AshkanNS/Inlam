50f6dd22529d3d50f5c777afbc09ed7a24471232
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:35:15 +0200

    Delete Kapitel4/Övningar directory

--- Kapitel4/Övningar/Övning4.4.py
+++
@@ -1,7 +0,0 @@
-#skriv en multiplikationstabell 
-
-
-for x in range (1, 10):
-    for j in range(1, 10):
-        print(f"{x * j:4}", end=" ")    # printen multiplicerar våra "for in range"
-    print()
