a18b35a804ee8627f4556ebc96f228f5b89d7ab8
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:34:55 +0200

    Delete Kapitel2/Övningar directory

--- Kapitel2/Övningar/Övning2.6.py
+++
@@ -1,8 +0,0 @@
-import math
-
-#kvarvarande radioaktivitet efter x antal år angivet i procen
-t = int(input("Antal år?"))
-lam = math.log (2.0) / 5730
-rest = 100 * math.exp(-lam * t)
-
-print (f"Det återstår {rest:.2f} %")
