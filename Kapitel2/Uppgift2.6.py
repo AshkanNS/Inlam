8f66404b2d55ac51e4e125d5d6c889e86de2cc99
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:19:55 +0200

    Delete Kapitel2/Uppgift2.6.py

--- Kapitel2/Uppgift2.6.py
+++
@@ -1,9 +0,0 @@
-#ange hur många sekunder som ska räknas
-sekunder = int(input("Ange sekunder: "))
-
-timmar = sekunder // 3600       #division för resultat i heltal
-restsekunder = sekunder % 3600  #restvärde av divisonen 
-minuter = restsekunder // 60    #
-sekunder = restsekunder % 60
-
-print (f"Det har det gått: {timmar} timmar, {minuter} minuter, {sekunder}sekunder")
