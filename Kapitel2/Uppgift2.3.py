5eb525f87456baa48be138a7b2a4b66777d5406a
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:19:28 +0200

    Delete Kapitel2/Uppgift2.3.py

--- Kapitel2/Uppgift2.3.py
+++
@@ -1,7 +0,0 @@
-#beräkning av kvadrat för heltal 
-
-tal = int(input("Skriv ett tal: "))
-#formel för beräkning av kvadrat
-kvadrat = tal**2
-#visar resultat
-print (f"Talet i kvadrat är: {kvadrat:.2f}")
