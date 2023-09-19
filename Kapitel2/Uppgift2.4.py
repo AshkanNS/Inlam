a572901fab876af173e353fc7f2ede9a44869533
AshkanNS <143608286+AshkanNS@users.noreply.github.com>
2023-09-19 18:19:37 +0200

    Delete Kapitel2/Uppgift2.4.py

--- Kapitel2/Uppgift2.4.py
+++
@@ -1,8 +0,0 @@
-#räkna ut omkrets samt arean på en kvadrat
-
-kvadratsida = int(input("längd"))
-omkrets = kvadratsida * 4
-area = kvadratsida * kvadratsida
-
-print ("Kvadratens omkrets: ", omkrets)
-print ("Kvadratens area: ", area)
